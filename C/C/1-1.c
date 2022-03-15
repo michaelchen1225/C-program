7c3d1afcedf3842be132e829a4ec97f3ff9ee325
michaelchen1225 <90543072+michaelchen1225@users.noreply.github.com>
2022-03-15 18:03:43 +0800

    Add files via upload
    315

---
+++ C/C/1-1.c
@@ -0,0 +1,11 @@
+/*prog1_1, 第一個C程式碼 */
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    printf("Hello!C!\n");    /*印出Hello C!*/
+    printf("Hello World!\n"); /*印出Hello World!*/
+
+    system("pause");
+    return 0;
+}
